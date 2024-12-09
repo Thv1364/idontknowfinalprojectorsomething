6a1ec9982a5021e07a1eff4fd78493055acb6045
Thv 1364 <jmonson6@outlook.com>
2024-12-09 19:26:31 +0000

    d

---
+++ no.py
@@ -0,0 +1,25 @@
+words=["chicken", "pie", "jonathanyoon", "jaydencheng", "topology"]
+import random
+word=random.choice(words)
+print(word)
+leng=len(word)
+print(leng)
+
+
+uknown
+
+
+def setup():
+    unknown=""
+    i=0
+    while i<leng:
+        unknown=unknown + "_"
+        i=i+1
+    return unknown    
+    
+uknown = setup()
+print(uknown)
+
+
+def check letter
+    
