bda0d6efa9f3b98247acf7849eefe70616feec76
Thv 1364 <jmonson6@outlook.com>
2024-12-10 05:29:24 +0000

    d

--- no.py
+++ no.py
@@ -1,25 +1,105 @@
-words=["chicken", "pie", "jonathanyoon", "jaydencheng", "topology"]
+words=["chicken"]#, "pie", "jonathanyoon", "jaydencheng", "topology"]
 import random
 word=random.choice(words)
-print(word)
 leng=len(word)
-print(leng)
-
-
-uknown
+splitword=list(word)
+allowedfails=0
 
 
 def setup():
-    unknown=""
+    unknown=[]
     i=0
     while i<leng:
-        unknown=unknown + "_"
+        unknown.append("_")
         i=i+1
     return unknown    
     
+def setupfails():
+    r=input("How many fails are you allowing? \n")
+    return r
+
+
 uknown = setup()
-print(uknown)
+combi=  ' '.join(uknown)
+print("Here is your solved word so far: " + combi)
+
+Letters=[]
+Lettersasked=0
+solved=0
+allowedfails=setupfails()
 
 
-def check letter
+def checkletter(lette):#check if letter is solved
+    k=0
+    woo=0
+    while k<leng:
+        if str(lette)==str(splitword[k]):
+            uknown[k]=lette
+
+            woo=1
+        k=k+1
+    if woo==0:
+        return 1
+    else:
+        return 0
+
+def returner(result):
+    if result==0:
+        return 1
+    else:
+        return 0#word not sp;ved
+
+def checkall():#check if whole word is solved
+    j=0
+    result=0
+    while j<leng:
+        if uknown[j]==splitword[j]:
+            result=result+0
+        else:
+            result=1
+
+        j=j+1
+    
+    return returner(result)
+    
+
+
+print("_______________________________________________ \n")
+
+
+##############################################################
+
+resul=0
+fails=0
+while resul==0: #main while loop
+    
+    if fails<=int(allowedfails):
+
+        if checkall()==0:
+            newletter = input("What is your letter? \n")
+            Letters.append(newletter)
+            print(int(checkletter(newletter)))
+            fails=int(fails) + int(checkletter(newletter))
+
+            combiword= ' '.join(uknown)
+            combilet=', '.join(Letters)
+            print("Here are the letters you have used: " + combilet)
+            print("Here is your solved word so far: " + combiword)
+            print("You have: " + str(fails) + " fails out of: " + str(allowedfails) + " allowed.")
+
+        else:#word is solved
+            print("Yay! You solved it!")
+            resul=1
+    else:
+        resul=2
+        print("loser")
+    print("_______________________________________________ \n")
+
+
+
+    Lettersasked=Lettersasked+1
+
+
+
+#def check letter
     
